39f90a1a26dde595b35f5780d179d7c60d49a753
dfamilia <dfamilia@intellisys.com.do>
2021-02-04 06:32:47 -0400

    add a shop page, creact a CollectionPreview and CollectionItem components

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import ShopPage from "./pages/shop";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
